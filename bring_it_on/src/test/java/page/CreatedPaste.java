090e8b173d1aec3fe83461122276e1e8922fbf5c
Sofiia Uchitel <territarrr@gmail.com>
2022-09-21 13:53:08 +0200

    Fix raw data bring_it_on

--- bring_it_on/src/test/java/page/CreatedPaste.java
+++ bring_it_on/src/test/java/page/CreatedPaste.java
@@ -5,15 +5,25 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import waits.CustomCondition;
+
+import java.time.Duration;
 
 public class CreatedPaste {
+    private final int WAIT_TIME_IN_SECONDS = 30;
     private final WebDriver driver;
+    @FindBy(xpath = "//div[contains(@class, 'top-buttons')]/div[contains(@class, 'right')]/a[contains(text(), 'raw')]")
+    private WebElement rawLink;
 
     @FindBy(xpath = "//div[contains(@class,'highlighted-code')]/descendant::a")
     private WebElement resultHighlighting;
 
     @FindBy(xpath = "//textarea[contains(@class,'js-paste-raw')]")
     private WebElement resultRawPaste;
+
+    @FindBy(xpath = "//pre")
+    private WebElement rawPaste;
 
     public CreatedPaste(WebDriver driver) {
         this.driver = driver;
@@ -29,7 +39,9 @@ public class CreatedPaste {
     }
 
     public String getRawPaste() {
-        return resultRawPaste.getText().trim();
+        rawLink.click();
+        String paste = rawPaste.getText().trim();
+        driver.navigate().back();
+        return paste;
     }
-
 }
